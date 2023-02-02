1994d0e22e9cf7ec6b3b980275851d5cddf6720e
gpapajim <g.papadimitriou.dev@gmail.com>
2023-02-02 12:06:50 +0200

    Scriptable oblect tile coloours

---
+++ Chess Game/Assets/Scripts/BoardColours.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TileColours",menuName = "TileColours")]
+public class BoardColours : ScriptableObject
+{
+    public Color first;
+    public Color second;
+}
