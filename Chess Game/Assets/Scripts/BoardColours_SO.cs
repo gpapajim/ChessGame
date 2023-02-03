643f9ab1b3a1da3289c396cf9fa20880f7186bfc
gpapajim <g.papadimitriou.dev@gmail.com>
2023-02-04 00:41:32 +0200

    Visual Studio prefered
    hopefully visual studio starts working as it should

--- Chess Game/Assets/Scripts/BoardColours_SO.cs
+++ Chess Game/Assets/Scripts/BoardColours_SO.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 [CreateAssetMenu(fileName = "TileColours",menuName = "TileColours")]
-public class BoardColours : ScriptableObject
+public class BoardColours_SO : ScriptableObject
 {
     public Color first;
     public Color second;
